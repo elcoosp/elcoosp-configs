4764d953838118553b9d1ad3d82282f1ebe56572
elcoosp <elcoosp@gmail.com>
2024-11-25 23:39:48 +0100

    chore(*): fmt

--- packages/rslib/src/index.ts
+++ packages/rslib/src/index.ts
@@ -1,11 +1,11 @@
 import type { RslibConfig } from '@rslib/core';
-import {readPackage} from 'read-pkg';
+import { readPackage } from 'read-pkg';
 export type Config = {
   preset: 'dual';
 };
 const presets = {
   dual: async () => {
-    const pkg = await readPackage()
+    const pkg = await readPackage();
     // TODO check that match inside package.json
     const cjsOutDistPathRoot = './dist/cjs';
     return {
@@ -27,7 +27,9 @@ const presets = {
     };
   },
 } as const satisfies Record<Config['preset'], () => Promise<RslibConfig>>;
-export async function createRsLibConfig({ preset }: Config): Promise<RslibConfig> {
+export async function createRsLibConfig({
+  preset,
+}: Config): Promise<RslibConfig> {
   // Should not call defineConfig otherwise build fail
-  return (await presets[preset]());
+  return await presets[preset]();
 }
