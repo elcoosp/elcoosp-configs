23189839e721749ec90cb90efcfdc5ef929ea296
elcoosp <elcoosp@gmail.com>
2024-11-25 14:55:29 +0100

    chore: mv workspace members to packages, rm semantic release to use changesets

--- release.config.mjs
+++
@@ -1,6 +0,0 @@
-/**
- * @type {import('semantic-release').GlobalConfig}
- */
-export default {
-  plugins: ["@semantic-release/commit-analyzer", "@semantic-release/release-notes-generator", "@semantic-release/npm"]
-}
