f40f2699207b1a61a406a079bf34750240d87f42
elcoosp <elcoosp@gmail.com>
2024-11-25 21:57:20 +0100

    chore(rslib): fmt

--- packages/rslib/rslib.config.ts
+++ packages/rslib/rslib.config.ts
@@ -1,3 +1,3 @@
 import { defineConfig } from "@rslib/core";
-import {createRsLibConfig} from './src/index'
-export default defineConfig(createRsLibConfig({preset:"dual"}));
+import { createRsLibConfig } from "./src/index";
+export default defineConfig(createRsLibConfig({ preset: "dual" }));
