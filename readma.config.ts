8bef1e016365876106094f1e7587586ffb9ea349
elcoosp <elcoosp@gmail.com>
2024-11-26 00:27:24 +0100

    fix: lock & lint & cspell

--- readma.config.ts
+++ readma.config.ts
@@ -1,32 +1,32 @@
-import { types } from "jsr:@readma/core";
-const name = "elcoosp-configs";
-const author = "elcoosp";
+import type { types } from 'jsr:@readma/core';
+const name = 'elcoosp-configs';
+const author = 'elcoosp';
 const config: types.ReadmeTemplateArgs = {
-  language: "ts",
-  title: "Configs",
+  language: 'ts',
+  title: 'Configs',
   author,
   githubUsername: author,
   repoName: name,
   xHandle: author,
-  domain: "gmail",
+  domain: 'gmail',
   email: author,
   urls: {
     doc: `https://${name}.vercel.app/`,
   },
-  repobeats: "b4ebcd827d58791b008f7bf5bddd78a2dd0e6a43",
-  images: { logo: "images/logo.png" },
+  repobeats: 'b4ebcd827d58791b008f7bf5bddd78a2dd0e6a43',
+  images: { logo: 'images/logo.png' },
   sections: {
-    projectDescription: "Monorepo of tools config files",
-    about: "Monorepo of tools config files",
-    installation: `See individual packages`,
-    acknowledgments: ``,
-    gettingStarted: "See screenshot",
-    roadmap: ``,
-    usage: ``,
+    projectDescription: 'Monorepo of tools config files',
+    about: 'Monorepo of tools config files',
+    installation: 'See individual packages',
+    acknowledgments: '',
+    gettingStarted: 'See screenshot',
+    roadmap: '',
+    usage: '',
   },
   template: {
-    bugReport: "bug-report--from-readme",
-    featRequest: "feature-request---from-readme",
+    bugReport: 'bug-report--from-readme',
+    featRequest: 'feature-request---from-readme',
   },
   backToTop: false,
 } as const;
