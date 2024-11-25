9f701bbb090a5754763a55f724cc050acf90f8f1
elcoosp <elcoosp@gmail.com>
2024-11-25 19:07:47 +0100

    fix: rslib config for cjs output

--- packages/docusaurus/rslib.config.ts
+++ packages/docusaurus/rslib.config.ts
@@ -1,19 +1,19 @@
-import { defineConfig } from '@rslib/core';
+import { defineConfig } from "@rslib/core";
 
 export default defineConfig({
-  lib: [
-    {
-      format: 'esm',
-      syntax: 'es2021',
-      dts: true,
-    },
-    {
-      format: 'cjs',
-      output: {
-        distPath: {
-          root: './dist/cjs',
-        },
-      },
-    },
-  ],
+	lib: [
+		{
+			format: "esm",
+			syntax: "es2021",
+			dts: true,
+		},
+		{
+			format: "cjs",
+			output: {
+				distPath: {
+					root: "./dist/cjs",
+				},
+			},
+		},
+	],
 });
